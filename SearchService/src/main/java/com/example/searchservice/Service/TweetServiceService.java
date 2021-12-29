e8a3c47cfb8b77450b6be0be3846f9d9b86bc19e
jaouharder <jaouharder@gmail.com>
2021-12-29 20:32:13 +0100

    added some details to searchService

---
+++ SearchService/src/main/java/com/example/searchservice/Service/TweetServiceService.java
@@ -0,0 +1,10 @@
+package com.example.searchservice.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+class SearchServiceService {
+
+   }
